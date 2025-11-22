edaf69a3c94c8be17e5835f423d7e95e09caff25
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-11-22 09:23:42 -0600

    update about page
    added about page text.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## About Music Pyramid
+Music Pyramid was born out of a simple frustration shared by many music enthusiastas: finding new bands that truly resonate with the sound of the artist you already love. In a household full of music lovers, discovering similar music and keeping track of it in one easy place, proved to be more difficult than expected. After researching existing solutions, it beame clear that while many application allow users to search for and Save music, few focus on helping listeners explore similar bands in an engaging and intuitive way. This inspired the create of Music Pyramid, an application deducated to helping users, listen to, and log new music based on the bands they already enjoy. 
+
+On the Similar Bands Page you can search for similar bands you already love. A list of similar bands will appear for users to explore. Every band you save is stored under your account so you can revisit them anytime in the My Bands page.
+
+## About This Application
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- pages/about.jsx
+++ pages/about.jsx
@@ -32,16 +32,37 @@ export default function About({ user, isLoggedIn }) {
       <Header isLoggedIn={isLoggedIn} username={user?.username} />
 
       <main className={styles.main}>
-        <h1>About This Application</h1>
+        <h1>About Music Pyramid</h1>
+
+        <h2>Inspiration</h2>
 
         <p>
-          This project helps users discover new music by finding bands similar
-          to the artists they already love. Every band you save is stored under
-          your account so you can revisit them anytime in the{" "}
+          Music Pyramid is built for listeners who want to dive deeper 
+          into their favorite genres and expand their personl music collection. The goal
+          is to take the frustration out of the discovery and replace it with a fun, intuitive
+          experience.
+          <br></br>
+          <br></br>
+          Most music appls let users search for artists or save their favorites, 
+          but very few made ti easy to explore truly similar bands in a meaningul, 
+          engaging way.l  After researcing this gap, the idea for Music Pyramid was born. 
+          <br></br>
+          <br></br>
+          <h2>Features</h2>
+          Starting with one band, users can build their own "music pyramid" as the application 
+          suggests similar artists to explore.  Each recommendation includes a band iage, name, 
+          a Wikipedia link for background, and a YouTube link to instantly sample their sound.
+          Every band you save is stored under your account so you can revisit them anytime in the{" "}
           <strong>My Bands</strong> page.
+          <br></br>
+          <br></br>
+          With Music Pyramid, discoverng your next favorite band becomes simple organized and exciting.
+          
+           
+          
         </p>
 
-        <h2>Features</h2>
+       
         
 
         <h2>Tech Stack</h2>
