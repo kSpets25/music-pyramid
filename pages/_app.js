9dcd27bb50e6ce65c21a610da3ba5fbcb903dc75
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-11-29 12:59:00 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
-import "./index.jsx"
+
+
+
 import React, {createContext,} from 'react';
 import '../styles/globals.css'
-import Header from "../components/header";
-
+import './index.jsx'
 
 export const AppContext = createContext() ;
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <>
-    <Component {...pageProps} />
-  </> 
- ); 
+  return <Component {...pageProps} />
+  
 }
 
 export default MyApp
